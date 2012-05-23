1eb764ff64604f830a82040672b9bf2f713b56de
Andrew Ittner <andrewi@cozi.com>
2012-05-23 09:29:05 -0700

    Properly figure dimensions and available pixels
    Do math once, then build random pixels using xrange instead of pulling
    from another image.

--- generate_random_image.py
+++ generate_random_image.py
@@ -11,17 +11,34 @@ import random
 from   PIL import Image, ImageColor
 
 
+def get_dimensions(requested_size_in_kilobytes):
+    """
+    Figure dimensions for given requested size.
+
+    Formula: target_size_in_bytes = height * width * 3
+
+    For a (mostly) square image, axis = sqrt(bytes/3)
+    """
+    size_in_bytes = requested_size_in_kilobytes * 1024
+    axis = long(math.sqrt(size_in_bytes/3))
+    # figure out how many pixels fit within this grid,
+    # since we rounded the figures
+    total_pixels = (axis * axis)
+
+    return dict(size_in_bytes=size_in_bytes,
+                axis_x=axis,
+                axis_y=axis,
+                total_pixels=total_pixels
+                )
+
 def generate_image_by_output_size(requested_size_in_kilobytes):
     """
     Generate a random bitmap by requested file size.
 
     Secret formula: 3 * HEIGHT * WIDTH == ~OUTPUT_SIZE_IN_BYTES
     """
-    # split height & width to make box
-    size_in_kb = long(requested_size_in_kilobytes) * 1024
-    axis = long(math.sqrt(size_in_kb/3))
-
-    size = (axis, axis)
+    dimensions = get_dimensions(requested_size_in_kilobytes)
+    size = (dimensions['axis_x'], dimensions['axis_y'])
     color = (255, 0, 0)
     img = Image.new("RGB", size, color)
     return img
@@ -30,11 +47,12 @@ def generate_random_image_by_size(requested_size_in_kilobytes, monochrome=False)
     """
     Generate a random image by size, optionally in monochrome
     """
-    img = generate_image_by_output_size(requested_size_in_kilobytes)
     img_data = []
     pixel_color = ""
     monochrome_colors = ("white", "black", )
-    for pixel in img.getdata():
+    #for pixel in img.getdata():
+    dimensions = get_dimensions(requested_size_in_kilobytes)
+    for x in xrange(0, dimensions['total_pixels']):
         if monochrome:
             pixel_color = random.choice(monochrome_colors)
             img_data.append(ImageColor.getrgb(pixel_color))
@@ -42,7 +60,7 @@ def generate_random_image_by_size(requested_size_in_kilobytes, monochrome=False)
             # randomize indices between 0 & 255
             img_data.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),))
 
-    randomized_image = Image.new("RGB", size=img.size)
+    randomized_image = Image.new("RGB", size=(dimensions['axis_x'], dimensions['axis_y']))
     randomized_image.putdata(img_data)
     return randomized_image
 
@@ -61,7 +79,7 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--monochrome', '-m', dest="monochrome", action="store_true", help="Make image black-and-white")
-    parser.add_argument('--size', '-s', dest="requested_size", help="Requested size, in kilobytes, of image")
+    parser.add_argument('--size', '-s', dest="requested_size", type=long, help="Requested size, in kilobytes, of image")
     parser.add_argument('--random', '-r', dest="randomize", action="store_true", help="Make a randomly-colored image")
     parser.add_argument('--output', '-f', dest='output_path', help="Output file")
 
